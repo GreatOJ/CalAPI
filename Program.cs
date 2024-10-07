ea896828937b13782ab66d30d02c831ef4c9b4d8
GreatOJ <beuchechi8@gmail.com>
2024-10-07 01:56:53 +0100

    Registered Services

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using CalAPI.Interface;
+using CalAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddTransient<ICalculatorService, CalculatorService>();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
