2dce919d9ec482d5bdd1b209d623c47b524e2ca1
GreatOJ <beuchechi8@gmail.com>
2024-10-07 01:35:30 +0100

    Created WEBAPI Project, Added Interface

---
+++ Interfaces/ICalculatorService.cs
@@ -0,0 +1,6 @@
+public interface ICalculatorService{
+    decimal Add(decimal x, decimal y);
+    decimal Subtract(decimal x, decimal y);
+    decimal Multiply(decimal x, decimal y);
+    decimal Divide(decimal x, decimal y);
+}
